f5ea3c058899c62c4ea0ddfe3254b2c1f3a83ac4
Benjamin Inglis <bjammin134@gmail.com>
2024-02-22 10:34:36 -0800

    Testing Snowflake Connector and data frames
    Successfully established a connection and pulled a sample data table, then stored it into a Pandas dataframe.

--- snowpark_session.py
+++ snowpark_session.py
@@ -1,13 +1,13 @@
-import snowflake.connector
 from snowflake.snowpark import Session #pip install snowflake-snowpark-python
 from snowflake.core import Root #pip install snowflake -U
 from dotenv import load_dotenv
 import os
 import pyarrow as pa #pip install pyarrow
 #pa.set_timezone_db_path("custom_path") #if using windows, uncomment this. See PyArrow documentation for installation
+root = Root(session)
 load_dotenv()
 
-connection_params = {
+conn_params = {
     'user': os.getenv('SNOWFLAKE_USER'),
     'password': os.getenv('SNOWFLAKE_PASSWORD'),
     'account': os.getenv('SNOWFLAKE_ACCOUNT'),
@@ -15,20 +15,8 @@ connection_params = {
     'database': os.getenv('SNOWFLAKE_DATABASE'),
     'schema': os.getenv('SNOWFLAKE_SCHEMA'),
 }
-# Establishing connection
-connection = snowflake.connector.connect(**connection_params)
 
-# Creating a cursor object
-cur = connection.cursor()
+new_session = Session.builder.configs(conn_params).create()
 
-try:
-    sql = "SELECT * FROM SAMPLE_PL_NE"
-    cur.execute(sql)
-
-    df = cur.fetch_pandas_all()
-    print(df)
-finally:
-    # Closing the cursor and connection
-    cur.close()
-    connection.close()
-
+#close the session when finished
+new_session.close()
